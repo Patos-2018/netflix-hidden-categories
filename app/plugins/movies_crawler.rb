88355ac557c106a749e6fe5cce73dcc57e88c08e
Damian Arquez <demian_ms@live.cl>
2018-06-23 22:20:24 -0400

    Rename TitlesCrawler to MoviesCrawler

--- app/plugins/movies_crawler.rb
+++ app/plugins/movies_crawler.rb
@@ -1,4 +1,4 @@
-class TitlesCrawler
+class MoviesCrawler
   def self.gather_titles
     agent = Mechanize.new
     page = agent.get('https://www.finder.com/cl/netflix-movies')
@@ -6,7 +6,7 @@ class TitlesCrawler
         .map(&:content)
   end
 
-  def self.populate_titles
+  def self.populate_database
     titles = gather_titles
     titles.each do |title|
       NetflixTitle.create!(name: title)
