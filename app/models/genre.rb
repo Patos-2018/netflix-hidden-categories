5ed41cf98e061fc4dd6f00d9f875bff778eccc9f
Damian Arquez <demian_ms@live.cl>
2018-06-24 20:06:10 -0400

    Add helper methods to Genre model and validations for NT code

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,13 @@
 class Genre < ApplicationRecord
   has_and_belongs_to_many :netflix_titles
+
+  def self.with_titles
+    joins(:netflix_titles).group('genres.id')
+                          .having('count(genre_id) > 0')
+  end
+
+  def self.with_no_titles
+    left_outer_joins(:netflix_titles).group('genres.id')
+                                     .having('count(genre_id) = 0')
+  end
 end
--- app/models/netflix_title.rb
+++ app/models/netflix_title.rb
@@ -2,6 +2,7 @@ class NetflixTitle < ApplicationRecord
   has_and_belongs_to_many :genres
   validates :name, presence: true
   validates :type, presence: true
+  validates :code, uniqueness: true
 
   def self.with_no_genres
     left_outer_joins(:genres).group('netflix_titles.id')
