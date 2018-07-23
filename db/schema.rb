3d113b33b196608c70d5d04b5ebf0b65c9c324d9
Carolina Contreras <ccre.caro@gmail.com>
2018-07-23 13:05:31 -0400

    add uri to genres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180623175947) do
+ActiveRecord::Schema.define(version: 20180723165935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180623175947) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uri"
   end
 
   create_table "genres_netflix_titles", id: false, force: :cascade do |t|
