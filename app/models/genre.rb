9ff1f5c70e171e3c0d574c98671d17c9aba0d575
Carolina Contreras <ccre.caro@gmail.com>
2018-07-23 15:58:16 -0400

    add callbacks counter

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,6 @@
 class Genre < ApplicationRecord
   has_and_belongs_to_many :netflix_titles
+  before_save :update_counter
 
   def self.with_titles
     joins(:netflix_titles).group('genres.id')
@@ -9,5 +10,9 @@ class Genre < ApplicationRecord
   def self.with_no_titles
     left_outer_joins(:netflix_titles).group('genres.id')
                                      .having('count(genre_id) = 0')
+  end
+
+  def update_counter
+    self.netflix_titles_count = netflix_titles.size
   end
 end
