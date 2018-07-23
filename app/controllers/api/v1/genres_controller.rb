7ebb44c9129dd044a0fb8eeb4f357e66582c5b8d
Mortup <g.uribemontero@gmail.com>
2018-07-23 16:42:43 +0000

    Added limit parameter to search

--- app/controllers/api/v1/genres_controller.rb
+++ app/controllers/api/v1/genres_controller.rb
@@ -3,7 +3,10 @@ module Api
     class GenresController < ApplicationController
 
       def index
-        @search = Genre.search {fulltext params[:w]}
+        @search = Genre.search do
+          fulltext params[:w]
+          paginate page: 1, per_page: params[:limit]
+        end
         render :json => @search.results.to_json( :only => [:name] )
       end
 
